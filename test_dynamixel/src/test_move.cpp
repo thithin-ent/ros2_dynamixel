a7b8654272f0a60e3530771c32ac3f7df7d860ac
thithin-ent <65296325+thithin-ent@users.noreply.github.com>
2021-02-17 12:56:47 +0900

    Update test_move.cpp

--- test_dynamixel/src/test_move.cpp
+++ test_dynamixel/src/test_move.cpp
@@ -1,5 +1,6 @@
 #include <rclcpp/rclcpp.hpp>
 #include "std_msgs/msg/int32.hpp"
+#include "std_msgs/msg/int16.hpp"
 #include "dynamixel_sdk/dynamixel_sdk.h"
 #include "math.h"
 
@@ -19,26 +20,37 @@ using namespace dynamixel;
 #define PROTOCOL_VERSION      2.0             // Default Protocol version of DYNAMIXEL X series.
 
 // Default setting
-#define DXL1_ID               1               // DXL1 ID
-#define DXL2_ID               2               // DXL2 ID
+#define DXL1_ID               4               // DXL1 ID
+#define DXL2_ID               3               // DXL2 ID
 #define BAUDRATE              57600           // Default Baudrate of DYNAMIXEL X series
 #define DEVICE_NAME           "/dev/ttyUSB0"  
 
 #define VALUE                 0
-#define OPERATING_VALUE       4               // 제어모드, 확장위치제어:4, 위치제어: 3, 속도제어: 1
+#define OPERATING_sposition   4               // 제어모드, 확장위치제어:4, 위치제어: 3, 속도제어: 1
+#define OPERATING_position    3  
 #define meter_tick            (48*M_PI/4096)
 
 
 PortHandler *portHandler;
 PacketHandler *packetHandler;
 
-void topic_callback(const std_msgs::msg::Int32::SharedPtr msg)
+void distance_callback(const std_msgs::msg::Int32::SharedPtr msg)
 {
   uint8_t dxl_error = 0;
   int move_distance = static_cast<int>(msg->data/meter_tick);
   packetHandler->write4ByteTxRx(portHandler, DXL2_ID, ADDR_GOAL_POSITION, move_distance ,&dxl_error);   //  -1,048,575 ~ 1,048,575까지 받기, 1당 0.088도
-  RCLCPP_INFO(node->get_logger(), "position: %d", move_distance);                                      //  mm를 토픽으로 받아 절대 위치를 기준으로 해당 거리만큼 이동
+  RCLCPP_INFO(node->get_logger(), "distance: %d", move_distance);                                      //  mm를 토픽으로 받아 절대 위치를 기준으로 해당 거리만큼 이동
 }
+
+
+void position_callback(const std_msgs::msg::Int16::SharedPtr msg)
+{
+  uint8_t dxl_error = 0;
+  packetHandler->write4ByteTxRx(portHandler, DXL1_ID, ADDR_GOAL_POSITION, msg->data ,&dxl_error);
+  RCLCPP_INFO(node->get_logger(), "position: %d", msg->data);
+}
+
+
 
 int main(int argc, char **argv)  
 {
@@ -58,15 +70,25 @@ int main(int argc, char **argv)
     RCLCPP_INFO(node->get_logger(),"Failed to set the baudrate!");
   }
 
-  packetHandler->write1ByteTxRx(portHandler, DXL2_ID, ADDR_OPERTAING_MODE, OPERATING_VALUE, &dxl_error);
-  packetHandler->write1ByteTxRx(portHandler, DXL2_ID, ADDR_TORQUE_ENABLE, 1, &dxl_error);
+  packetHandler->write1ByteTxRx(portHandler, DXL1_ID, ADDR_OPERTAING_MODE, OPERATING_position, &dxl_error); //  eeprom 입력
+  packetHandler->write1ByteTxRx(portHandler, DXL2_ID, ADDR_OPERTAING_MODE, OPERATING_sposition, &dxl_error); //    // 
+ 
+  packetHandler->write1ByteTxRx(portHandler, DXL1_ID, ADDR_TORQUE_ENABLE, 1, &dxl_error);                    //   토크잠금
+  packetHandler->write1ByteTxRx(portHandler, DXL2_ID, ADDR_TORQUE_ENABLE, 1, &dxl_error);                    //     //
+  
   RCLCPP_INFO(node->get_logger(),"start setting");
+  packetHandler->write4ByteTxRx(portHandler, DXL1_ID, ADDR_GOAL_POSITION, VALUE, &dxl_error);
   packetHandler->write4ByteTxRx(portHandler, DXL2_ID, ADDR_GOAL_POSITION, VALUE, &dxl_error);
 
-  auto subscription =
-    node->create_subscription<std_msgs::msg::Int32>("/topic", 10, topic_callback);
+  auto subscription1 =
+    node->create_subscription<std_msgs::msg::Int32>("/move_distance", 10, distance_callback);
+  
+  auto subscription2 =
+    node->create_subscription<std_msgs::msg::Int16>("/move_position", 10, position_callback);
+  
 
   rclcpp::spin(node);
+  packetHandler->write1ByteTxRx(portHandler, DXL1_ID, ADDR_TORQUE_ENABLE, 0, &dxl_error);
   packetHandler->write1ByteTxRx(portHandler, DXL2_ID, ADDR_TORQUE_ENABLE, 0, &dxl_error);
   rclcpp::shutdown();
   
